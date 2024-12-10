6c49de9051e7f6f927ad81eabdd77047043319f5
Alaguraj1 <alaguraj259@gmail.com>
2024-12-10 18:53:54 +0530

    changes

--- app/(homes)/home-1/page.jsx
+++ app/(homes)/home-1/page.jsx
@@ -2,10 +2,11 @@ import Footer2 from "@/components/footers/Footer2";
 import Header17 from "@/components/headers/Header17";
 import Hero from "@/components/homes/home-18/Hero";
 import Service from "@/components/homes/home-2/Service";
+import HomeService from "@/components/service/HomeServices";
 import React from "react";
 
 export const metadata = {
-  title: "Home 18 || Industry - Factory & Industrial React Nextjs Template",
+  title: "Home || Zing Studio",
   description: "Industry - Factory & Industrial React Nextjs Template",
 };
 export default function page() {
@@ -16,7 +17,7 @@ export default function page() {
         <div className="page-content bg-white">
           <Hero />
           <div className="content-block">
-            <Service />
+            <HomeService />
           </div>
         </div>
         <Footer2 />
