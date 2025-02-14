e7c165b0e3faeb45aa9570739a8bcd586ce5465e
nsathiyakala <nsathiyakala6@gmail.com>
2025-02-14 17:30:12 +0530

    iimage changes

--- components/service/Am-a-zing.jsx
+++ components/service/Am-a-zing.jsx
@@ -135,7 +135,7 @@ export default function Am_a_Zing() {
                   <img
                   alt=""
                   className="img-cover img-inner"
-                  src="/images/Home-Page/amazing/image.jpg"
+                  src="/images/Home-Page/amazing/am-a-zing-img-1.jpg"
                   style={{
                       width:"300px",
                       height:"800px",
@@ -149,7 +149,7 @@ export default function Am_a_Zing() {
                   <img
                   alt=""
                   className="img-cover img-inner"
-                  src="/images/Home-Page/amazing/image.jpg"
+                  src="/images/Home-Page/amazing/am-a-zing-img-2.jpg"
                   style={{
                       width:"300px",
                       height:"800px",
