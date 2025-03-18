8f33d5973758ca6c68efe17df280827cf820a76a
nsathiyakala <nsathiyakala6@gmail.com>
2025-03-18 09:54:55 +0530

    service font color

--- components/service/LightVersion.jsx
+++ components/service/LightVersion.jsx
@@ -47,9 +47,9 @@ const LightVersion = ({
                         <ul className="list-details">
                         <li className="text-black d-flex flex-sm-row justify-content-sm-between flex-column justify-content-start ">
                             <span   style={{ fontSize: "18px" , fontWeight: "600" }}>Web Design</span>
-                            <span className="pt-1 pt-sm-0" style={{ fontSize: "18px", fontWeight: "600 " }}>Starting @ ₹ 8750</span>
+                            <span className="pt-1 pt-sm-0" style={{  fontWeight: "500", fontSize:"17px", color:"#e21100" }}>Starting @ ₹ 8750</span>
                         </li>
-                        <li className="pt-3 pb-0 text-black" style={{ fontWeight: "600",borderTop:"1px solid black" }}>Static Websites</li>
+                        <li className="pt-3 pb-0 text-black" style={{ fontWeight: "600",borderTop:"1px solid black"}}>Static Websites</li>
                         <ul className="list-arrow mb-0 mt-0">
                       <li className="text-black px-4" style={{ borderTop: "none", color: "white" }}>1 Home Page Design</li>
                       <li className="text-black px-4" style={{ borderTop: "none", color: "white" }}>Upto 10 Inner Page Design</li>
@@ -72,7 +72,7 @@ const LightVersion = ({
                         <ul className="list-details">
                         <li className="text-black d-flex flex-sm-row justify-content-sm-between flex-column justify-content-start ">
                             <span style={{ fontSize: "18px" , fontWeight: "600" }}>Web Design</span>
-                            <span  className="pt-1 pt-sm-0" style={{ fontSize: "18px", fontWeight: "600" }}>Starting @ ₹ 25,850</span>
+                            <span  className="pt-1 pt-sm-0" style={{  fontWeight: "500", fontSize:"17px", color:"#e21100" }}>Starting @ ₹ 25,850</span>
                         </li>
                         <li className="pt-3 pb-0 text-black" style={{ fontWeight: "600" , borderTop:"1px solid black"}}>CMS Websites</li>
                         <ul className="list-arrow mb-0 mt-0">
@@ -98,7 +98,7 @@ const LightVersion = ({
                         <ul className="list-details">
                         <li className="text-black d-flex flex-sm-row justify-content-sm-between flex-column justify-content-start ">
                             <span style={{ fontSize: "18px", fontWeight: "600"  }}>Web Design</span>
-                            <span  className="pt-1 pt-sm-0" style={{ fontSize: "18px" ,  fontWeight: "600" }}>Starting @ ₹ 10,000</span>
+                            <span  className="pt-1 pt-sm-0" style={{  fontWeight: "500", fontSize:"17px", color:"#e21100" }}>Starting @ ₹ 10,000</span>
                         </li>
                         <li className="pt-3 pb-0 text-black" style={{ fontWeight: "600", borderTop:"1px solid black" }}>E-Commerce Websites</li>
                         <li className="text-black mt-0 pt-0" style={{ borderTop: "none" }}>
@@ -127,7 +127,7 @@ const LightVersion = ({
                         <ul className="list-details">
                         <li className="text-black d-flex flex-sm-row justify-content-sm-between flex-column justify-content-start ">
                             <span style={{ fontSize: "18px" , fontWeight: "600" }}>Digital Marketing</span>
-                            <span className="pt-1 pt-sm-0" style={{ fontSize: "18px", fontWeight: "600"  }}>Starting @ ₹ 25,999</span>
+                            <span className="pt-1 pt-sm-0" style={{  fontWeight: "500", fontSize:"17px", color:"#e21100" }}>Starting @ ₹ 25,999</span>
                         </li>
                         <li className="pt-3 pb-0 text-black" style={{ fontWeight: "600",borderTop:"1px solid black" }}>Basic</li>
                         <ul className="list-arrow mb-0 mt-0">
@@ -152,7 +152,7 @@ const LightVersion = ({
                         <ul className="list-details">
                         <li className="text-black d-flex flex-sm-row justify-content-sm-between flex-column justify-content-start ">
                             <span style={{ fontSize: "18px" , fontWeight: "600" }}>Digital Marketing</span>
-                            <span className="pt-1 pt-sm-0" style={{ fontSize: "18px", fontWeight: "600" }}>Starting @ ₹ 35,000</span>
+                            <span className="pt-1 pt-sm-0" style={{  fontWeight: "500", fontSize:"17px", color:"#e21100" }}>Starting @ ₹ 35,000</span>
                         </li>
                         <li className="pt-3 pb-0 text-black" style={{ fontWeight: "600",borderTop:"1px solid black" }}>Standard</li>
                         <ul className="list-arrow mb-0 mt-0">
@@ -177,7 +177,7 @@ const LightVersion = ({
                         <ul className="list-details">
                         <li className="text-black d-flex flex-sm-row justify-content-sm-between flex-column justify-content-start ">
                             <span style={{ fontSize: "18px", fontWeight: "600"  }}>Digital Marketing</span>
-                            <span className="pt-1 pt-sm-0" style={{ fontSize: "18px" ,  fontWeight: "600" }}>Starting @ ₹ 25,850</span>
+                            <span className="pt-1 pt-sm-0" style={{  fontWeight: "500", fontSize:"17px", color:"#e21100" }}>Starting @ ₹ 25,850</span>
                         </li>
                         <li className="pt-3 pb-0 text-black" style={{ fontWeight: "600" , borderTop:"1px solid black"}}>Premium</li>
                         <li className="text-black mt-0 pt-0" style={{ borderTop: "none" }}>
